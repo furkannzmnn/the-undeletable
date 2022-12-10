17dfe47253affa37ce5edff6751c27f699cacca4
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-12-11 00:02:08 +0300

    connection setting start

--- src/main/java/org/infa/Main.java
+++ src/main/java/org/infa/Main.java
@@ -10,10 +10,9 @@ public class Main {
         // bu uygulamayı 9093 portunda çalıştır
 
 
-        Executors.newCachedThreadPool()
-                .submit(() ->
+
                         new ConnectionFactory()
-                        .connect("/Users/furkanozmen/Desktop/the-undeletable/src/main/java/org/infa/unDeletable.yml"));
+                        .connect("/Users/furkanozmen/Desktop/the-undeletable/src/main/java/org/infa/unDeletable.yml");
 
 
         Thread.sleep(3000);
